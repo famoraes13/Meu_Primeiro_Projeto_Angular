4aad76e1c3c050a33e339c56dd5df3e89ddf0245
famoraes13 <62120531+famoraes13@users.noreply.github.com>
2021-09-16 21:30:41 -0300

    Alteração no html do component ola-angular

---
+++ projeto-angular/src/app/ola-angular/ola-angular.component.ts
@@ -0,0 +1,16 @@
+
+import { style } from "@angular/animations";
+import { CssSelector } from "@angular/compiler";
+import { Component } from "@angular/core";
+
+@Component({
+    
+    selector:"ola-angular",
+    templateUrl:"./ola-angular.component.html" ,
+    styleUrls:['./ola-angular.component.css']
+
+ })
+ 
+ export class OlaAngularComponent{
+       framework ="Angular"; 
+}
