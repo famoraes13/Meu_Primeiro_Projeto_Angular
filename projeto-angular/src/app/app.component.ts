235498df121932a6f71aa09c540bf742dcd4d84d
famoraes13 <62120531+famoraes13@users.noreply.github.com>
2021-09-21 04:27:01 -0300

    Commit 1

--- projeto-angular/src/app/app.component.ts
+++ projeto-angular/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { log } from 'console';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'projeto-angular';
-}
+  nome="";
+  alterarNome(nome: { target: { value: string; }; }) {
+    console.log(nome.target.value);
+    this.nome = nome.target.value;
+    }
+  adicionar(nomeInput: any) { 
+  console.log("Adicionado novo nme : " + nomeInput);
+    console.log("Adicionando...")
+    }
+  }
+
